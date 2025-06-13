98044f18670298008853a2686a0bbbabe6ba7c8d
Harshil Singh <hars.sin.hil@gmail.com>
2025-06-13 10:06:45 +0000

    Update executor.h to use query.h

--- src/executor/executor.h
+++ src/executor/executor.h
@@ -5,33 +5,32 @@
 #include <string>
 #include <vector>
 
-#include "../utils/trades.h"
+#include "../utils/query.h"
 
 /**
  * @class Executor
  * @brief Handles execution of trade data queries including price metrics and raw data retrieval.
  */
-class Executor {
-public:    
+class Executor
+{
+public:
     /**
      * @brief Computes the lowest and highest prices within the given query window.
      *
      * @param query  A TradeDataQuery specifying the symbol, time window, resolution, and metrics.
      * @return A vector of pairs of price structs representing the lowest and highest prices found.
      */
-    std::vector<Result> lowest_and_highest_prices(TradeDataQuery& query);
+    std::vector<Result> lowest_and_highest_prices(TradeDataQuery &query);
     /**
      * @brief Retrieves raw trade data corresponding to the query parameters.
      *
      * @param query  A TradeDataQuery defining the criteria for raw data selection.
      * @return A vector of TradeData objects matching the query range and filters.
      */
-    std::vector<TradeData> send_raw_data (TradeDataQuery& query);
-
+    std::vector<TradeData> send_raw_data(TradeDataQuery &query);
 
 private:
-  // Any Internal members can be added here
-    
+    // Any Internal members can be added here
 };
 
-#endif 
+#endif
