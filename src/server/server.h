5236c21a2828f5bcbbd077819fa0f3ab2abbf44f
manogyasingh <manogyasinghsuryansh@gmail.com>
2025-06-13 14:46:27 +0530

    implement server connection functions

--- src/server/server.h
+++ src/server/server.h
@@ -3,20 +3,19 @@
 #include <sys/epoll.h>
 #include "../utils/net/net.h"
 
+void make_non_blocking(int sock);
 class EpollServer {
  public:
   EpollServer(int port);
   ~EpollServer();
 
   void run();
-
- private:
+  
+  private:
   sockaddr_in server_address_;
   int server_listen_fd_;
   int epoll_fd_;
   void accept_connection();
-
-  void add_to_epoll();
-  void make_non_blocking();
+  void add_to_epoll(int sock);
   void bind_server();
 };
