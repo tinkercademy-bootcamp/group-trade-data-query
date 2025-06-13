cecddb904be862924dccf6dd3a9a8ba786c31eeb
Rajasree Ponnada <user@ip-172-31-25-255.ap-south-1.compute.internal>
2025-06-13 06:25:52 +0000

    feat: created Price struct

--- src/utils/query.h
+++ src/utils/query.h
@@ -7,3 +7,12 @@ struct TradeDataQuery {
   uint64_t resolution;   // 0 to mean raw data feed
   uint64_t metrics;      // bit flag
 };
+
+/**
+ * @struct price
+ * @brief Represents a price with its associated exponent for decimal scaling.
+ */
+struct price {
+  uint32_t price;
+  int8_t price_exponent;
+};
