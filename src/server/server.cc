0af573a638eea60cbb72f6d9b1d333b420d696f3
manogyasingh <manogyasinghsuryansh@gmail.com>
2025-06-13 14:46:27 +0530

    implement server connection functions

--- src/server/server.cc
+++ src/server/server.cc
@@ -1,47 +1,56 @@
 #include "server.h"
-// #include "../utils/helper/utils.h"
+#include "../utils/helper/utils.h"
 #include "../utils/net/net.h"
-
 
 EpollServer::EpollServer(int port)
     : server_listen_fd_(net::create_socket()),
-      server_address_(net::create_address(port)) {
-        make_non_blocking();
-        bind_server();
-        add_to_epoll();
-      }
-
-EpollServer::~EpollServer() {}
-
-void EpollServer::run() {}
-
-void EpollServer::accept_connection() {}
-
-void EpollServer::add_to_epoll() {
-  epoll_fd_ = epoll_create1(0);
-  if (epoll_fd_ == -1) {
-    throw std::runtime_error("Failed to create epoll file descriptor: " + std::string(strerror(errno)));
-  }
-
-  epoll_event event;
-  event.data.fd = server_listen_fd_;
-  event.events = EPOLLIN | EPOLLET; // Edge-triggered, read events
-
-  if (epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, server_listen_fd_, &event) == -1) {
-    throw std::runtime_error("Failed to add server socket to epoll: " + std::string(strerror(errno)));
-  }
+      server_address_(net::create_address(port)),
+      epoll_fd_(epoll_fd_ = epoll_create1(0)) {
+  make_non_blocking(server_listen_fd_);
+  bind_server();
+  add_to_epoll(server_listen_fd_);
 }
 
+EpollServer::~EpollServer() {
+  close(server_listen_fd_);
+  close(epoll_fd_);
+}
 
-void EpollServer::make_non_blocking() {
-  int flags = fcntl(server_listen_fd_, F_GETFL, 0);
-  if (flags != -1) {
-    fcntl(server_listen_fd_, F_SETFL, flags | O_NONBLOCK);
-  }
+void EpollServer::run() {
+  helper::check_error(listen(server_listen_fd_, SOMAXCONN) < 0,
+                      "Failed to listen on server socket");
+}
+
+void EpollServer::accept_connection() {
+  sockaddr_in client_addr;
+  socklen_t client_len = sizeof(client_addr);
+  int client_fd =
+      accept(server_listen_fd_, reinterpret_cast<sockaddr*>(&client_addr),
+             &client_len);
+  helper::check_error(client_fd < 0, "Failed to accept connection");
+  make_non_blocking(client_fd);
+  add_to_epoll(client_fd);
+}
+
+void EpollServer::add_to_epoll(int sock) {
+  epoll_event event{};
+  event.data.fd = sock;
+  event.events = EPOLLIN | EPOLLET;  // Edge-triggered, read events
+
+  helper::check_error(
+      epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, sock, &event) == -1,
+      "Failed to add client socket to epoll");
+}
+
+void make_non_blocking(int sock) {
+  int flags = fcntl(sock, F_GETFL, 0);
+  helper::check_error(flags == -1, "Failed to get socket flags");
+  helper::check_error(fcntl(sock, F_SETFL, flags | O_NONBLOCK) == -1, "Failed to set socket to non-blocking");
 }
 
 void EpollServer::bind_server() {
-  if (bind(server_listen_fd_, reinterpret_cast<sockaddr*>(&server_address_), sizeof(server_address_)) < 0) {
-    throw std::runtime_error("Failed to bind server socket: " + std::string(strerror(errno)));
-  }
+  helper::check_error(
+      bind(server_listen_fd_, reinterpret_cast<sockaddr*>(&server_address_),
+           sizeof(server_address_)) < 0,
+      "Failed to bind server socket");
 }
