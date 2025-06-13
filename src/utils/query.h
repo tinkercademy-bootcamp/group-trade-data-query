07e3b231131a12ddc978c561dc46aeb4987c49ea
Rajasree Ponnada <user@ip-172-31-25-255.ap-south-1.compute.internal>
2025-06-13 06:25:52 +0000

    feat: created Price struct

--- src/utils/query.h
+++ src/utils/query.h
@@ -3,9 +3,18 @@
 
 // TradeDataQuery is a structure used to query trade data for a specific asset symbol over a defined time range.
 struct TradeDataQuery {
-  uint32_t symbol_id;           //Symbol ID of the asset
-  uint64_t start_time_point;    // Start time point in nanoseconds
-  uint64_t end_time_point;      // End time point in nanoseconds
-  uint64_t resolution;          // 0 to mean raw data feed, in nanoseconds. It denotes the time interval of the data points.
-  uint64_t metrics;             // bit flag which will be used to determine which metrics to return, for example min_max, mean, stddev, etc.
+  uint32_t symbol_id;
+  uint64_t start_time_point;
+  uint64_t end_time_point;
+  uint64_t resolution;   // 0 to mean raw data feed
+  uint64_t metrics;      // bit flag
+};
+
+/**
+ * @struct price
+ * @brief Represents a price with its associated exponent for decimal scaling.
+ */
+struct price {
+  uint32_t price;
+  int8_t price_exponent;
 };
