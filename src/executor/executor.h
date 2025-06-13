1ba54b55d615737ad6d6510b823b59ebed6fd56d
Rajasree Ponnada <user@ip-172-31-25-255.ap-south-1.compute.internal>
2025-06-13 06:29:07 +0000

    chore: clean redundant code

--- src/executor/executor.h
+++ src/executor/executor.h
@@ -7,15 +7,13 @@
 
 #include "../utils/query.h"
 
+/**
+ * @class Executor
+ * @brief Handles execution of trade data queries including price metrics and raw data retrieval.
+ */
 class Executor {
-public:
-    struct price {
-      uint32_t price;
-      int8_t price_exponent;
-    };
-    // function to find lowest and highest prices with argument as a struct query uint32_t Symbol Id, uint64_t start_time_point; uint64_t end_time_point; uint64_t resolution;   // 0 to mean raw data feed; uint64_t metrics;      // bit flag returning the metrics as std::vector with std::pair
+public:    
     std::vector<std::pair<price, price>> lowest_and_highest_prices(TradeDataQuery& query);
-    // fucntion to return the raw_data when the resolution is 0, i.e., the vetcor of structs just called
     std::vector<TradeDataQuery> send_raw_data (TradeDataQuery& query);
 
 
