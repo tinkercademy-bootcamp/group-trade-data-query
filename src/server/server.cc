acd0b4bbe5089197ba1601e77f2bdce52298745c
manogyasingh <manogyasinghsuryansh@gmail.com>
2025-06-13 13:53:15 +0530

    implement offload queue for outbound data transfers

--- src/server/server.cc
+++ src/server/server.cc
@@ -1,16 +1,12 @@
 #include "server.h"
-//#include "../utils/helper/utils.h"
+// #include "../utils/helper/utils.h"
 #include "../utils/net/net.h"
 
 EpollServer::EpollServer(int port)
     : server_listen_fd_(net::create_socket()),
       server_address_(net::create_address(port)) {
-  // set_non_blocking(server_listen_fd_);
-  // opt_bind_listen();
-  epoll_fd_ = epoll_create1(0);
-  // check_error(epoll_fd_ < 0, "Couldn't make epoll socket");
-  // add_to_epoll(server_listen_fd_, EPOLLIN | EPOLLET);
-}
+        make_non_blocking();
+      }
 
 EpollServer::~EpollServer() {}
 
@@ -19,3 +15,16 @@ void EpollServer::run() {}
 void EpollServer::accept_connection() {}
 
 void EpollServer::add_to_epoll() {}
+
+void EpollServer::make_non_blocking() {
+  int flags = fcntl(server_listen_fd_, F_GETFL, 0);
+  if (flags != -1) {
+    fcntl(server_listen_fd_, F_SETFL, flags | O_NONBLOCK);
+  }
+}
+
+void EpollServer::bind_server() {
+  if (bind(server_listen_fd_, reinterpret_cast<sockaddr*>(&server_address_), sizeof(server_address_)) < 0) {
+    throw std::runtime_error("Failed to bind server socket: " + std::string(strerror(errno)));
+  }
+}
