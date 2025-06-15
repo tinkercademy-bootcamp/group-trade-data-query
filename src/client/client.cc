cd7e363e418c46b029f716edea19f5f98e9760ba
Aryan Maskara <aryan.maskara@gmail.com>
2025-06-15 22:30:09 +0000

    Fix indentation to 2 spaces in client

--- src/client/client.cc
+++ src/client/client.cc
@@ -7,37 +7,37 @@
 #include <arpa/inet.h>
 
 client::Client::Client(uint16_t port, const std::string &server_address)
-    : socket_{net::create_socket()} {
-    sockaddr_in address = create_server_address(server_address, port);
-    connect_to_server(socket_, address);
+  : socket_{net::create_socket()} {
+  sockaddr_in address = create_server_address(server_address, port);
+  connect_to_server(socket_, address);
 }
 
 void client::Client::send_message(const TradeDataQuery &message) {
     
-    ssize_t bytes_sent = send(socket_, &message, sizeof(message), 0);
-    if (bytes_sent < 0) {
-        helper::check_error(true, "Send failed on client socket.");
-    }
+  ssize_t bytes_sent = send(socket_, &message, sizeof(message), 0);
+  if (bytes_sent < 0) {
+    helper::check_error(true, "Send failed on client socket.");
+  }
 }
 
 int32_t client::Client::get_socket_fd() const {
-    return socket_;
+  return socket_;
 }
 client::Client::~Client() { close(socket_); }
 
 sockaddr_in client::Client::create_server_address(
-    const std::string &server_ip, uint16_t port) {
-    sockaddr_in address = net::create_address(port);
-    // Convert the server IP address to a binary format
-    auto err_code = inet_pton(AF_INET, server_ip.c_str(), &address.sin_addr);
-    helper::check_error(err_code <= 0, "Invalid address/ Address not supported\n");
-    return address;
+  const std::string &server_ip, uint16_t port) {
+  sockaddr_in address = net::create_address(port);
+  // Convert the server IP address to a binary format
+  auto err_code = inet_pton(AF_INET, server_ip.c_str(), &address.sin_addr);
+  helper::check_error(err_code <= 0, "Invalid address/ Address not supported\n");
+  return address;
 }
 
 void client::Client::connect_to_server(
-    int32_t sock, sockaddr_in &server_address) {
-    int32_t err_code =connect(sock, (sockaddr *)&server_address, sizeof(server_address));
-    helper::check_error(err_code < 0, "Connection Failed.\n");
+  int32_t sock, sockaddr_in &server_address) {
+  int32_t err_code =connect(sock, (sockaddr *)&server_address, sizeof(server_address));
+  helper::check_error(err_code < 0, "Connection Failed.\n");
 }
 
 std::vector<Result> client::Client::read_min_max() {
