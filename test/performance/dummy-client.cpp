3f35df84aa3d8566259befdf89f8d27ead2c094f
Dinu Goyal <user@ip-172-31-17-176.ap-south-1.compute.internal>
2025-06-15 14:45:02 +0000

    Modify dummy-client to accomodate varying delays, for the tests

--- test/performance/dummy-client.cpp
+++ test/performance/dummy-client.cpp
@@ -2,12 +2,22 @@
 #include <string>
 #include <thread>
 #include <chrono>
+#include <random>
 
 int main() {
     std::string input;
-    while (std::getline(std::cin, input)) {
-        std::this_thread::sleep_for(std::chrono::seconds(5));  // Sleep for 5 seconds
-        std::cout << input << "_processed" << std::endl;
-    }
+    std::getline(std::cin, input);
+    
+    // Simulate some processing time (random between 1-50ms)
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 50);
+    int delay_ms = dis(gen);
+    
+    std::this_thread::sleep_for(std::chrono::milliseconds(delay_ms));
+    
+    // Simply append "_processed" to the input
+    std::cout << input << "_processed" << std::endl;
+    
     return 0;
 }
