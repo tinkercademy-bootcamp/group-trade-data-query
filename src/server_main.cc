e964459080652f3e16aa89d67574c02375bdbaf7
Balaji <balajikaredla4@gmail.com>
2025-06-17 01:33:54 +0530

    add file as argument to server

--- src/server_main.cc
+++ src/server_main.cc
@@ -1,10 +1,14 @@
 #include "server/server.h"
 #include <spdlog/spdlog.h>  
 
-int32_t main(){
+int32_t main(int argc, char** argv) {
+  if (argc < 2) {
+    spdlog::error("Usage: {} <filename>", argv[0]);
+    return 1;
+  }
   int32_t kPort;
   kPort = 8080;
-  EpollServer server(kPort);
+  EpollServer server(kPort, argv[1]);
   spdlog::info("Server started on port {}", kPort);
   server.run();
   return 0;
