b7c0362ef83d8a1133bfe216ef7357d7970aa1bc
Balaji <balajikaredla4@gmail.com>
2025-06-17 01:07:53 +0530

    rename executor to query_engine

--- process_data/make_page_table.cc
+++ process_data/make_page_table.cc
@@ -10,7 +10,7 @@
 #include "../src/query_engine/page_table.h"
 
 int main() {
-    Query_engine executor;
+    Query_engine query_engine;
     std::ofstream out("data/processed/page-table-example.bin", std::ios::binary);
 
     if (!out.is_open()) {
@@ -21,8 +21,8 @@ int main() {
     int64_t ind = 0;
     TradeData trade;
     std::cout << sizeof(TradeData) << " bytes per trade" << std::endl;
-    while (ind < executor.trades_size) {
-        executor.read_trade_data(ind, trade);
+    while (ind < query_engine.trades_size) {
+        query_engine.read_trade_data(ind, trade);
         out.write(reinterpret_cast<const char*>(&trade.created_at), sizeof(uint64_t));
         ind += (1 << 17);
     }
