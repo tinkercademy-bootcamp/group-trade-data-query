c33cca3407fb2fd4e8d2f4381eacd09745560e50
Rishika Garg <user@ip-172-31-18-95.ap-south-1.compute.internal>
2025-06-16 11:26:05 +0000

    sync udp with all the changes happening

---
+++ src/server_udp/sender_udp.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <cstring>
+#include <iostream>
+#include <queue>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "../utils/query.h"
+
+class OffloadQueue {
+ public:
+  void serialise_and_enqueue(int32_t id, const std::string& final_result);
+  void offload_one();
+
+ private:
+  std::queue<std::pair<int32_t, std::string>> queue_;
+
+  std::pair<int32_t, std::string> front() { return queue_.front(); }
+  void push(const std::pair<int32_t, std::string>& data) { queue_.push(data); }
+  void pop() { queue_.pop(); }
+  bool empty() const { return queue_.empty(); }
+};
+
+std::string serialise(const std::string& final_result);
+void send_data(int32_t sockfd, const std::string& data);
+void send_without_serialisation(int32_t sockfd, Result& result,
+                                const sockaddr_in& client_addr);
+void send_without_serialisation(int32_t sockfd, TradeData& data,
+                                const sockaddr_in& client_addr);
