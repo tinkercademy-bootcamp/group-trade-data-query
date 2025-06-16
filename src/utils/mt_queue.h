f4e3acefd0f06f24109accbfbd31f79e28fa70c3
Harshil Singh <hars.sin.hil@gmail.com>
2025-06-16 06:26:22 +0000

    Move Mt_Queue template functions to header file

--- src/utils/mt_queue.h
+++ src/utils/mt_queue.h
@@ -2,6 +2,7 @@
 #include <memory>
 #include <mutex>
 #include <semaphore>
+#include <queue>
 
 #define LEAST_MAX_QSIZE 10000
 
@@ -9,8 +10,25 @@ template <typename T>
 class Mt_Queue
 {
 public:
-  std::unique_ptr<T> pop();
-  void push(std::unique_ptr<T> ptr);
+  Mt_Queue() : qsize(0) {}
+
+  std::unique_ptr<T> pop()
+  {
+    qsize.acquire();
+    qlock.acquire();
+    std::unique_ptr<T> ptr = std::move(_queue.front());
+    _queue.pop();
+    qlock.release();
+    return ptr;
+  }
+
+  void push(std::unique_ptr<T> ptr)
+  {
+    qlock.acquire();
+    _queue.push(std::move(ptr));
+    qlock.release();
+    qsize.release();
+  }
 
 private:
   std::mutex qlock;
