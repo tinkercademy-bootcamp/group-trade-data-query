54546c5f01b3b19c2db62978961bebe3a6ae2f00
logan <user@ip-172-31-13-82.ap-south-1.compute.internal>
2025-06-17 21:32:25 +0000

    Remove compilation errors from make_page_tables.cc

--- process_data/make_page_table.cc
+++ process_data/make_page_table.cc
@@ -7,7 +7,14 @@
 #include <cstring>
 #include "../src/utils/query.h"
 #include "../src/query_engine/query_engine.h"
-#include "../src/query_engine/page_table.h"
+
+std::ifstream data;
+
+bool read_trade_data(uint64_t ind, TradeData& trade) {
+  data.seekg(ind * sizeof(TradeData), std::ios::beg);
+  data.read(reinterpret_cast<char *>(&trade), sizeof(TradeData));
+  return true; // If we reach here, it means reading failed
+}
 
 int32_t main(int32_t argc, char** argv) {
     if (argc < 2) {
@@ -16,8 +23,14 @@ int32_t main(int32_t argc, char** argv) {
         return 1;
     }
     std::string filename = argv[1];
-    Query_engine query_engine(filename);
+    // Query_engine query_engine(filename);
     std::ofstream outer_page_table_bin("data/processed/page-table-" + filename + "-outer.bin", std::ios::binary);
+
+    data.open("data/processed/trades-" + filename + ".bin", std::ios::in | std::ios::binary);
+    if (!data.is_open()) {
+        std::cerr << "[Query_engine] Error: could not open trade data file.\n";
+        return 0;
+    }
     
     if (!outer_page_table_bin.is_open()) {
         std::cerr << "Error: could not open output file for writing." << std::endl;
@@ -31,12 +44,14 @@ int32_t main(int32_t argc, char** argv) {
         return 1;
     }
 
+    uint64_t trades_size = data.seekg(0, std::ios::end).tellg() / sizeof(TradeData);
+
     int64_t ind = 0;
     TradeData trade;
-    while (ind < query_engine.trades_size) {
-        query_engine.read_trade_data(ind, trade);
+    while (ind < trades_size) {
+        read_trade_data(ind, trade);
         outer_page_table_bin.write(reinterpret_cast<const char*>(&trade.created_at), sizeof(uint64_t));
-        if (__builtin_ctzll(i) >= 26) {
+        if (__builtin_ctzll(ind) >= 26) {
             inner_page_table_bin.write(reinterpret_cast<const char*>(&trade.created_at), sizeof(uint64_t));
         }
         ind += (1 << 17);
